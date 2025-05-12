7471f7c08a2935c3f510a9d28ffef6ad60cfb784
Chaim Shlez <chaimshlez@gmail.com>
2025-05-12 15:04:33 +0300

    Add project files.

---
+++ SeriesAnalyzer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void ManegerMenu(bool isValid);
+        
+
+        private static void insertNumbersByUser();
+       
+
+        private static void Menu();
+        
+
+
+        //function is valid
+        public static bool ValidateNums(string[] args);
+
+        public void GetByInserted(int[] nums);
+
+        public void GetAverage(int[] nums);
+        public void GetMax(int[] nums);
+
+        public void GetMin(int[] nums);
+        public void GetSum(int[] nums);
+
+        public void GetNumbersSorted(int[] nums);
+        public void GetNumbersSorted(int[] nums);
+        public void GetNumbersReversed(int[] nums);
+        public void GetCountNumbers(int[] nums);
+
+    }
+}
