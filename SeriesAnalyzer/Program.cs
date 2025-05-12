d513677661063b8fc26e56bc6e6a7d4ca8453a84
Chaim Shlez <chaimshlez@gmail.com>
2025-05-12 18:19:06 +0300

    functions-by-choice

--- SeriesAnalyzer/Program.cs
+++ SeriesAnalyzer/Program.cs
@@ -106,6 +106,7 @@ namespace SeriesAnalyzer
         //function that receives numbers from the user
         private static void insertNumbersByUser()
         {
+            numbers.Clear();
             Console.WriteLine("please enter numbers with spases between them");
             string input = Console.ReadLine();
 
@@ -163,19 +164,97 @@ namespace SeriesAnalyzer
 
         }
 
-        public static void GetByInserted(List<int> numbers) { }
+        public static void GetByInserted(List<int> numbers) {
+            foreach(int number in numbers)
+            {
+               printNumByChoice(number);
+            }
+        }
 
-        public static void GetAverage(List<int> numbers) { }
-        public static void GetMax(List<int> numbers) { }
+        public static void GetAverage(List<int> numbers) {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            int average = sum / numbers.Count;
+            printNumByChoice(average);  
+        }
+        public static void GetMax(List<int> numbers) {
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Count; i++) {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            printNumByChoice(max);
+        }
 
 
-        public static void GetMin(List<int> numbers) { }
-        public static void GetSum(List<int> numbers) { }
+        public static void GetMin(List<int> numbers) {
+            int min = numbers[0];
+            for (int i= 1; i < numbers.Count; i++) {
+                if (numbers[i] < min) {
+                    min = numbers[i];
+                }
+            }
+            printNumByChoice(min);
+        }
+        public static void GetSum(List<int> numbers) {
 
-        public static void GetNumbersSorted(List<int> numbers) { }
-        
-        public static void GetNumbersReversed(List<int> numbers) { }
-        public static void GetCountNumbers(List<int> numbers) { }
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++) {
+                sum += numbers[i];
+            }
+            printNumByChoice(sum);
+        }
+
+        public static void GetNumbersSorted(List<int> numbers) {
+            List<int> sortSrr = new List<int>(numbers);
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                for(int j =i+1;j<numbers.Count; j++)
+                {
+                    if (numbers[i] > numbers[j]) {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                            numbers[j] = temp; 
+                    }
+
+                }
+            
+            }
+            printArrSorted(sortSrr);
+        }
+
+        private static void printArrSorted(List<int> sortSrr)
+        {
+            foreach (int number in sortSrr)
+            {
+                printNumByChoice(number);
+            }
+        }
+
+        public static void GetNumbersReversed(List<int> numbers) { 
+           for(int i =numbers.Count - 1; i >= 0; i--)
+            {
+                printNumByChoice(numbers[i]);
+            }
+        }
+        public static void GetCountNumbers(List<int> numbers) { 
+            printNumByChoice(numbers.Count);
+        }
+
+
+
+        public static void printNumByChoice(int num) { 
+
+         Console.WriteLine(num);
+        }
 
     }
 }
